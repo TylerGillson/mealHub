691b62a1edf617b0981fcc1736edb8da291b4fcd
Tyler Gillson <tyler.gillson@gmail.com>
2017-03-07 16:59:41 -0700

    Added User model for site users.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    user_type = models.CharField(max_length=5)
+    regdate = models.DateTimeField()
+
+    def __str__(self):
+        return self.username
