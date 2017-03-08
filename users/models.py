69f9772b9c27b810954259e72fa6acb21f3de76a
Richard Sterling <rsterlin@mavs.coloradomesa.edu>
2017-03-07 22:00:20 -0700

    Added type user model, now you can only select between Chef/Mouth

--- users/models.py
+++ users/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    TYPE_CHOICES = (
+        ('C',"Chef"),
+        ('M',"Mouth"),
+    )
+
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    user_type = models.CharField(max_length=5)
+    user_type = models.CharField(max_length=5, choices = TYPE_CHOICES)
     regdate = models.DateTimeField()
 
     def __str__(self):
