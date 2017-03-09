f0385d1a90691b22a3f5bb724560605c90dfb32b
Richard Sterling <rsterlin@mavs.coloradomesa.edu>
2017-03-09 15:21:49 -0700

    Updated models.py with Some more fields to User and added MealRequest, Ingredient, Meal. Also Messed with views.py and cannot get that to work.

--- users/views.py
+++ users/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.template import loader
+
+
+from .models import MealRequest, User
 
 # Create your views here.
 class  LoginView(generic.ListView):
@@ -10,7 +14,15 @@ class  LoginView(generic.ListView):
 
 
 def ChefsView(request):
-    return render(request, 'users/chef.html')
+    mealrequest = MealRequest.objects.all()
+    return render(request, 'users/chef.html', {'mealrequest':mealrequest})
+
+#>>> for req in MealRequest.objects.all():
+#...     print("%s requests %d servings of %s" %(req.user.username, req.servings_requested, req.mealRequestName)). 
+#tyler requests 4 servings of Butter Chicken
 
 def MouthsView(request):
     return render(request, 'users/mouth.html')
+
+def RegisterView(request):
+    return render(request, 'users/register.html')
