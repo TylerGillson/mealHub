c0fa01787d5cb6a506c8bf9eda35395fdc4ac9e1
Richard Sterling <rsterlin@mavs.coloradomesa.edu>
2017-03-23 14:03:44 -0700

    started basic feed for search page--may actually not be useful

--- meals/urls.py
+++ meals/urls.py
@@ -6,3 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^search/$', views.SearchView, name ='search'),
 ]
+
+#want to create a url pattern like ../user_name/meal_name
--- meals/views.py
+++ meals/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 
+from .models import Meal
+
 def SearchView(request):
-    return render(request, 'meals/search.html')
+    meal = Meal.objects.order_by('-date_available')
+    context = {'meal': meal,}
+    return render(request, 'meals/search.html', context)
