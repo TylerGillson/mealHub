c07e072a9a7a3cd1d43390052cdfd5e1198b2792
Richard Sterling <rsterlin@mavs.coloradomesa.edu>
2017-03-27 18:11:03 -0600

    added mealRequest form, implimeted UserHubView to display dependign on user view

--- meals/views.py
+++ meals/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Meal
+from users.models import MealRequest
 
 def SearchView(request):
     meal = Meal.objects.order_by('-date_available')
-    context = {'meal': meal,}
+    meal_request = MealRequest.objects.order_by('-date_requested')
+    context = {'meal': meal,'meal_request': meal_request, }
     return render(request, 'meals/search.html', context)
