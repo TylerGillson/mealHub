36d76e20f8f76741f0b779249d02362df42a407d
Tyler Gillson <tyler.gillson@gmail.com>
2017-04-05 20:57:19 -0600

    updated todolist, db users, models.py

--- mealhub/models.py
+++ mealhub/models.py
@@ -4,12 +4,22 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 import datetime
 
+TYPE_CHOICES = (
+    ('C', "Chef"),
+    ('M', "Mouth"),
+)
+
+RATING_VALUE = (
+    (0, "No Stars"),
+    (1, "One Star"),
+    (2, "Two Stars"),
+    (3, "Three Stars"),
+    (4, "Four Stars"),
+    (5, "Five Stars"),
+)
+
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    TYPE_CHOICES = (
-        ('C', "Chef"),
-        ('M', "Mouth"),
-    )
     user_type = models.CharField(max_length=1, choices=TYPE_CHOICES)
     zip_code = models.IntegerField(default = 00000)
 
@@ -17,22 +27,14 @@ class Profile(models.Model):
         return self.user.username
 
 class Meal(models.Model):
-    RATING_VALUE = (
-        (0, "No Stars"),
-        (1, "One Star"),
-        (2, "Two Stars"),
-        (3, "Three Stars"),
-        (4, "Four Stars"),
-        (5, "Five Stars"),
-    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     mealname = models.CharField(max_length=50)
     mealdesc = models.CharField(max_length=200)
+    meal_rating = models.IntegerField(choices=RATING_VALUE, default=0)
     #need to fix the date
     date_posted = models.DateTimeField(auto_now_add=True)
     date_available = models.DateTimeField(auto_now_add=True)
     servings_available = models.IntegerField(null=True)
-    meal_rating = models.IntegerField(choices=RATING_VALUE, default=0)
     photo = models.ImageField(upload_to='meals/%Y/%m/%d', blank=True, default='meals/None/noimg.jpg')
 
     def __str__(self):
@@ -41,15 +43,18 @@ class Meal(models.Model):
 class Ingredient(models.Model):
     meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
     ingredient_name = models.CharField(max_length=25)
-   # quantity = models.DecimalField(max_digits=4, decimal_places=2) #don't think i really need this
 
     def __str__(self):
         return self.ingredient_name
 
 class Review(models.Model):
     meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    review_rating = models.IntegerField(choices=RATING_VALUE, default=0)
     reviewed_by = models.ForeignKey(User, on_delete=models.CASCADE)
     review_text = models.TextField(max_length=255)
+
+    def __str__(self):
+        return str(self.reviewed_by)+'-'+str(self.meal)
 
 class MealRequest(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -57,7 +62,7 @@ class MealRequest(models.Model):
     date_created = models.DateTimeField(default =timezone.now)
     date_requested = models.DateTimeField(default =timezone.now)
     servings_requested = models.IntegerField(null=True)
-    other = models.CharField(max_length=200) #I'm alergic to peanuts.. etc
+    other = models.CharField(max_length=200)
 
     def __str__(self):
         return self.mealRequestName
