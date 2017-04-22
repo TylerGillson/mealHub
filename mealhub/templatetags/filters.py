a2485733dba77b1e8514fab66451d651651c4db8
Shawn Pacarar <spacarar@hotmail.com>
2017-04-22 10:41:24 -0600

    made markers clickable to most recent request or meal

--- mealhub/templatetags/filters.py
+++ mealhub/templatetags/filters.py
@@ -34,24 +34,40 @@ def times(number):
 
 @register.assignment_tag
 def meal_addrs():
-    meals = Meal.objects.all()
+    meals = Meal.objects.order_by('-id')
     profiles = Profile.objects.all()
     meal_addrs = []
     for meal in meals:
         for p in profiles:
             if meal.user == p.user and p.address is not None:
-                meal_addrs.append((p.user,p.address + " " + p.city + " " + p.state + " " + str(p.zip_code)))
+                fixedName = meal.user.username.replace('.','')
+                fixedMeal = meal.mealname.replace(' ','')
+                meal_addrs.append((p.user,p.address + " " + p.city + " " + p.state + " " + str(p.zip_code),fixedMeal,fixedName))
     meal_addrs = set(meal_addrs)
-    return meal_addrs
+    users = []
+    newMealAddrs = []
+    for x in meal_addrs:
+        if x[0] not in users:
+            newMealAddrs.append(x)
+            users.append(x[0])
+    return newMealAddrs
 
 @register.assignment_tag
 def meal_request_addrs():
-    meal_requests = MealRequest.objects.all()
+    meal_requests = MealRequest.objects.order_by('-id')
     profiles = Profile.objects.all()
+    fixedName = meal_requests[0].user.username.replace('.','')
+    fixedRequest = meal_requests[0].mealRequestName.replace(' ','')
     meal_request_addrs = []
     for meal_request in meal_requests:
         for p in profiles:
             if meal_request.user == p.user and p.address is not None:
-                meal_request_addrs.append((p.user,p.address + " " + p.city + " " + p.state + " " + str(p.zip_code)))
+                meal_request_addrs.append((p.user,p.address + " " + p.city + " " + p.state + " " + str(p.zip_code),fixedRequest,fixedName))
     meal_request_addrs = set(meal_request_addrs)
-    return meal_request_addrs
+    users = []
+    newMealRequestAddrs = []
+    for x in meal_request_addrs:
+        if x[0] not in users:
+            newMealRequestAddrs.append(x)
+            users.append(x[0])
+    return newMealRequestAddrs
